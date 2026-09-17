4ffdf90943847b13d065a17ae7026b4b9e309089
WhimmyAlz <AlfredZhang1234@gmail.com>
2026-09-17 18:06:37 -0400

    added fetchmobs and killmobs commands to the dev tool

--- Global/dev.gd
+++ Global/dev.gd
@@ -1,12 +1,18 @@
 extends Node2D
 
 var console_text
-var command_lib = {"stats" : 0, "setHealth" : 1, "setStamina" : 1, "spawn" : 2} # has a command name then a 
+var command_lib = {"stats" : 0, "setHealth" : 1, "setStamina" : 1, "spawn" : 2, "help" : 0, "fetchMobs" : 0, "killMobs" : 1} # has a command name then a 
+var auto_close = true
 
 ## Closes and clears the console
 func close():
 	$LineEdit.clear()
 	$".".hide()
+
+## Clears but doesn't close the console
+func nc_clear():
+	auto_close = false
+	$LineEdit.clear()
 
 ## Opens and begins editing the console
 func open():
@@ -36,7 +42,7 @@ func console_return_and_collect():
 				
 				# if command_usage is successful, return true, else false
 				return_val = command_usage(command)
-				if return_val == true:
+				if return_val and auto_close:
 					close()
 				else:
 					$LineEdit.edit()
@@ -72,6 +78,12 @@ func command_usage(txt):
 		return(set_stamina(command))
 	elif command[0] == "spawn":
 		return(spawn(command))
+	elif command[0] == "help":
+		return(help(command))
+	elif command[0] == "fetchMobs":
+		return(fetch_mobs(command))
+	elif command[0] == "killMobs":
+		return(kill_mobs(command))
 
 ## returns true or false based on correct amounts of arguments
 func arg_check(commands, args):
@@ -84,6 +96,8 @@ func arg_check(commands, args):
 ## Displays some basic game stats
 func stats(command):
 	var correct_args = arg_check(command, 0)
+	nc_clear()
+	
 	if correct_args:
 		log_text("FPS: {fps}".format({"fps" : Engine.get_frames_per_second()}))
 		return(true)
@@ -93,6 +107,7 @@ func stats(command):
 ## Sets the health of the player
 func set_health(command):
 	var correct_args = arg_check(command, 1)
+	auto_close = true
 
 	if correct_args:
 		var valid = command[1].is_valid_float()
@@ -109,7 +124,8 @@ func set_health(command):
 ## Sets the health of the player
 func set_stamina(command):
 	var correct_args = arg_check(command, 1)
-	
+	auto_close = true
+
 	if correct_args:
 		var valid = command[1].is_valid_float()
 		if float(command[1]) <= 0.0 or not valid:
@@ -125,7 +141,8 @@ func set_stamina(command):
 ## Spawns an enemy
 func spawn(command):
 	var correct_args = arg_check(command, 2)
-	
+	auto_close = true
+
 	var skelebone = preload("res://Scenes/Mobs/Skelebone/skelebone.tscn")
 	var nerd = preload("res://Scenes/Mobs/Nerd/nerd.tscn")
 	var sir_blob = preload("res://Scenes/Bosses/Sir Blob/sir_blob.tscn")
@@ -153,11 +170,67 @@ func spawn(command):
 	else:
 		return(false)
 
+## shows all commands
+func help(command):
+	var correct_args = arg_check(command, 0)
+	nc_clear()
+
+	if correct_args:
+		log_text("Here are all the commands:\n%s" % [command_lib.keys()])
+		return(true)
+	else:
+		return(false)
+
+## spawns an enemy at (0, 0)
 func spawn_enemy(enemy_preload, lvl):
 	var enemy = enemy_preload.instantiate()
 	enemy.set_pos(Vector2(0, 0))
 	enemy.Level = int(lvl)
 	get_tree().current_scene.get_node("Enemies").add_child(enemy)
+
+## gathers all mobs in the enemies node and outputs in console
+func fetch_mobs(command):
+	var correct_args = arg_check(command, 0)
+	nc_clear()
+
+	if correct_args:
+		var enemy_list = get_tree().current_scene.get_node("Enemies")
+		log_text("Here are all the enemies in order:")
+		for i in range(enemy_list.get_child_count()):
+			log_text("Index %d: %s" % [i, str(enemy_list.get_child(i))])
+		return(true)
+	else:
+		return(false)
+
+## kills mobs in enemies node based on index provided or all mobs if parameter is All
+func kill_mobs(command):
+	var correct_args = arg_check(command, 1)
+	auto_close = true
+	
+	var enemy_list = get_tree().current_scene.get_node("Enemies")
+	var valid = false
+
+	if correct_args:
+		if command[1].is_valid_int():
+			if enemy_list.get_child_count()-1 >= int(command[1]):
+				valid = true
+			else:
+				valid = false
+		elif command[1] == "All":
+			valid = true
+
+		if valid and command[1] != "All":
+			log_text("Killed %s" % enemy_list.get_child(int(command[1])))
+			enemy_list.get_child(int(command[1])).on_death()
+			return(true)
+		elif valid and command[1] == "All":
+			for i in range(enemy_list.get_child_count()):
+				log_text("Killed %s" % enemy_list.get_child(i))
+				enemy_list.get_child(i).on_death()
+			return(true)
+		else:
+			log_text("Error: Bad argument type. Requires valid index Argument. Use fetchMobs command for indexes or use All argument.")
+			return(false)
 
 func _ready() -> void:
 	$Console_log.scroll_following = true
